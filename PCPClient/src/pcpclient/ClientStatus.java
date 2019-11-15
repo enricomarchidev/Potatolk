0d7a49021c15b7ed5046e17a921ccd7587d1ab56
enricomarchidev <ienrico01@gmail.com>
2019-11-15 13:49:41 +0100

    Implementazione del metodo registration

--- PCPClient/src/pcpclient/ClientStatus.java
+++ PCPClient/src/pcpclient/ClientStatus.java
@@ -5,10 +5,44 @@
  */
 package pcpclient;
 
+import java.net.Socket;
+
 /**
  *
  * @author Enrico Marchi
  */
 public class ClientStatus {
     
+    public void registration(String alias, String topic){
+        // creazione del pacchetto PCP
+        byte[] packet = new byte[2048];
+        
+        // indice pacchetto
+        int index = 0;
+        
+        // opcode
+        packet[index++] = 10;
+   
+        // version
+        packet[index++] = 0;
+        
+        // alias
+        for (byte b: alias.getBytes())
+            packet[index++] = b;
+        
+        // byte separatore
+        packet[index++] = 0;
+        
+        //topic
+        for (byte b: topic.getBytes())
+            packet[index++] = b;
+        
+        // byte separatore
+        packet[index++] = 0;
+        
+        //creazione della connessione TCP con il server
+        Socket client = new Socket(my_address,53101); // creazione ClientSocket su porta 13
+        
+        
+    }
 }
