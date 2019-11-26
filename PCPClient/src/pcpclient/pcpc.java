84c914d5a8d81c9c50e5bd51a13053f3704548e6
GRIPPI GIULIO FEDERICO <17841@EDU.MARCONI>
2019-11-26 11:47:44 +0100

    added logout window

---
+++ PCPClient/src/pcpclient/pcpc.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pcpclient;
+
+/**
+ *
+ * @author 17841
+ */
+class pcpc {
+    
+}
